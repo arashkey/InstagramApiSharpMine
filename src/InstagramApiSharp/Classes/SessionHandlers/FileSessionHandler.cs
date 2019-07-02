bdbd39cf9ab318b4ac3b5b7ab6091d83cefc555d
Ramtin Jokar <ramtinak@live.com>
2019-07-03 01:28:40 +0430

    Remove UWP support from ISessionHandler

--- src/InstagramApiSharp/Classes/SessionHandlers/FileSessionHandler.cs
+++ src/InstagramApiSharp/Classes/SessionHandlers/FileSessionHandler.cs
@@ -4,57 +4,57 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-#if WINDOWS_UWP
-using Windows.Storage;
-#endif
+//#if WINDOWS_UWP
+//using Windows.Storage;
+//#endif
 namespace InstagramApiSharp.Classes.SessionHandlers
 {
     public class FileSessionHandler : ISessionHandler
     {
 
         public IInstaApi InstaApi { get; set; }
-#if WINDOWS_UWP
-        /// <summary>
-        ///     File => Optional
-        ///     <para>If you didn't set this, InstagramApiSharp will choose it automatically based on <see cref="InstaApi"/> username!</para>
-        /// </summary>
-        public StorageFile File { get; set; }
+//#if WINDOWS_UWP
+//        /// <summary>
+//        ///     File => Optional
+//        ///     <para>If you didn't set this, InstagramApiSharp will choose it automatically based on <see cref="InstaApi"/> username!</para>
+//        /// </summary>
+//        public StorageFile File { get; set; }
 
-        private readonly StorageFolder LocalFolder = ApplicationData.Current.LocalFolder;
-#else
+//        private readonly StorageFolder LocalFolder = ApplicationData.Current.LocalFolder;
+//#else
         /// <summary>
         ///     Path to file
         /// </summary>
         public string FilePath { get; set; }
-#endif
+//#endif
 
         /// <summary>
         ///     Load and Set StateData to InstaApi
         /// </summary>
         public
-#if WINDOWS_UWP
-            async
-#endif
+//#if WINDOWS_UWP
+//            async
+//#endif
             void Load()
         {
-#if WINDOWS_UWP
-            if (File == null)
-                throw new ArgumentNullException("File cannot be null for Load session\r\nPlease set a file and try again.");
-            try
-            {
-                var json = await FileIO.ReadTextAsync(File);
-                if (string.IsNullOrEmpty(json))
-                    return;
+//#if WINDOWS_UWP
+//            if (File == null)
+//                throw new ArgumentNullException("File cannot be null for Load session\r\nPlease set a file and try again.");
+//            try
+//            {
+//                var json = await FileIO.ReadTextAsync(File);
+//                if (string.IsNullOrEmpty(json))
+//                    return;
 
-                if(InstaApi == null)
-                    InstaApi = InstaApiBuilder.CreateBuilder()
-                        .SetUser(UserSessionData.Empty)
-                        .Build();
+//                if(InstaApi == null)
+//                    InstaApi = InstaApiBuilder.CreateBuilder()
+//                        .SetUser(UserSessionData.Empty)
+//                        .Build();
 
-                InstaApi.LoadStateDataFromString(json);
-            }
-            catch { }
-#else
+//                InstaApi.LoadStateDataFromString(json);
+//            }
+//            catch { }
+//#else
             if (File.Exists(FilePath))
             {
                 using (var fs = File.OpenRead(FilePath))
@@ -62,25 +62,25 @@ namespace InstagramApiSharp.Classes.SessionHandlers
                     InstaApi.LoadStateDataFromStream(fs);
                 }
             }
-#endif
+//#endif
         }
 
         /// <summary>
         ///     Save current StateData from InstaApi
         /// </summary>
         public
-#if WINDOWS_UWP
-            async
-#endif
+//#if WINDOWS_UWP
+//            async
+//#endif
             void Save()
         {
-#if WINDOWS_UWP
-            if (File == null)
-                File = await LocalFolder.CreateFileAsync($"{InstaApi.GetLoggedUser().UserName.ToLower()}.bin", CreationCollisionOption.ReplaceExisting);
+//#if WINDOWS_UWP
+//            if (File == null)
+//                File = await LocalFolder.CreateFileAsync($"{InstaApi.GetLoggedUser().UserName.ToLower()}.bin", CreationCollisionOption.ReplaceExisting);
 
-            var json = InstaApi.GetStateDataAsString();
-            await FileIO.WriteTextAsync(File, json, Windows.Storage.Streams.UnicodeEncoding.Utf8);
-#else
+//            var json = InstaApi.GetStateDataAsString();
+//            await FileIO.WriteTextAsync(File, json, Windows.Storage.Streams.UnicodeEncoding.Utf8);
+//#else
             using (var state = InstaApi.GetStateDataAsStream())
             {
                 using (var fileStream = File.Create(FilePath))
@@ -89,7 +89,7 @@ namespace InstagramApiSharp.Classes.SessionHandlers
                     state.CopyTo(fileStream);
                 }
             }
-#endif
+//#endif
         }
     }
 }
