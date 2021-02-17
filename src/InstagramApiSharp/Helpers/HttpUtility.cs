71f8b662eb61c55de471803916e69cd9c578421c
ramtin <ramtinak@live.com>
2021-02-18 03:05:56 +0330

    Add ParseQueryString(string query, out string type) to HttpUtility

--- src/InstagramApiSharp/Helpers/HttpUtility.cs
+++ src/InstagramApiSharp/Helpers/HttpUtility.cs
@@ -22,5 +22,17 @@ namespace InstagramApiSharp.Helpers
                             .ToDictionary(grouping => grouping.Key,
                                           grouping => string.Join(",", grouping));
         }
+
+        public static Dictionary<string, string> ParseQueryString(string query, out string type)
+        {
+            type = query.Split('?')[0];
+            return query.Split('?')[1]
+                            .Split(new[] { '&', ';' }, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(parameter => parameter.Split(new[] { '=' }, StringSplitOptions.RemoveEmptyEntries))
+                            .GroupBy(parts => parts[0],
+                                     parts => parts.Length > 2 ? string.Join("=", parts, 1, parts.Length - 1) : (parts.Length > 1 ? parts[1] : ""))
+                            .ToDictionary(grouping => grouping.Key,
+                                          grouping => string.Join(",", grouping));
+        }
     }
 }
