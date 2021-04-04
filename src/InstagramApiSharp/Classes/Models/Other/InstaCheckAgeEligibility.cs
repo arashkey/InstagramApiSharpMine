35d344eb3a456b97039a654142673b9bb42debfc
ramtin <ramtinak@live.com>
2021-04-04 19:50:06 +0430

    Add InstaCheckAgeEligibility

---
+++ src/InstagramApiSharp/Classes/Models/Other/InstaCheckAgeEligibility.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Developer: Ramtin Jokar [ Ramtinak@live.com ] [ My Telegram Account: https://t.me/ramtinak ]
+ * 
+ * Github source: https://github.com/ramtinak/InstagramApiSharpMine
+ * 
+ * IRANIAN DEVELOPERS (c) 2021, April
+ */
+
+using InstagramApiSharp.Classes.Models;
+using Newtonsoft.Json;
+
+namespace InstagramApiSharp.Classes
+{
+    public class InstaCheckAgeEligibility : InstaDefaultResponse
+    {
+        [JsonProperty("eligible_to_register")]
+        public bool? EligibleToRegister { get; set; }
+
+        [JsonProperty("parental_consent_required")]
+        public bool? ParentalConsentRequired { get; set; }
+    }
+}
