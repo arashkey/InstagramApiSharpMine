c2ca56b53db065355b95a236a4bbc1c19e3891d5
ramtin <ramtinak@live.com>
2021-04-04 19:45:51 +0430

    Add InstaRegistrationMethod

---
+++ src/InstagramApiSharp/Enums/InstaRegistrationMethod.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Developer: Ramtin Jokar [ Ramtinak@live.com ] [ My Telegram Account: https://t.me/ramtinak ]
+ * 
+ * Github source: https://github.com/ramtinak/InstagramApiSharpMine
+ * 
+ * IRANIAN DEVELOPERS (c) 2021, Apr
+ */
+
+namespace InstagramApiSharp.Enums
+{
+    public enum InstaRegistrationMethod
+    {
+        Email,
+        Phone
+    }
+}
