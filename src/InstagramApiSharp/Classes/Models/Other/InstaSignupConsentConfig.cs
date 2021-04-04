826f995c4ce8dabade1088200fd6d8efaf435f1d
ramtin <ramtinak@live.com>
2021-04-04 19:48:18 +0430

    Add InstaSignupConsentConfig

---
+++ src/InstagramApiSharp/Classes/Models/Other/InstaSignupConsentConfig.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Developer: Ramtin Jokar [ Ramtinak@live.com ] [ My Telegram Account: https://t.me/ramtinak ]
+ * 
+ * Github source: https://github.com/ramtinak/InstagramApiSharpMine
+ * 
+ * IRANIAN DEVELOPERS 2021, April
+ */
+
+using InstagramApiSharp.Classes.Models;
+using Newtonsoft.Json;
+
+namespace InstagramApiSharp.Classes
+{
+    public class InstaSignupConsentConfig : InstaDefaultResponse
+    {
+        [JsonProperty("age_required")]
+        public bool? AgeRequired { get; set; }
+
+        [JsonProperty("gdpr_required")]
+        public bool? GdprRequired { get; set; }
+
+        [JsonProperty("tos_acceptance_not_required")]
+        public bool? TosAcceptanceNotRequired { get; set; }
+    }
+}
