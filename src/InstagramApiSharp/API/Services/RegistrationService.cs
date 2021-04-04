dab5b3b4ab0defbe3be2fb4d52535ea4e4f6b1be
ramtin <ramtinak@live.com>
2021-04-04 20:05:50 +0430

    Add IRegistrationService and RegistrationService

---
+++ src/InstagramApiSharp/API/Services/RegistrationService.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Developer: Ramtin Jokar [ Ramtinak@live.com ] [ My Telegram Account: https://t.me/ramtinak ]
+ * 
+ * Github source: https://github.com/ramtinak/InstagramApiSharpMine
+ * 
+ * IRANIAN DEVELOPERS
+ */
+
+using InstagramApiSharp.Classes;
+using InstagramApiSharp.Classes.Android.DeviceInfo;
+using InstagramApiSharp.Classes.Models;
+using InstagramApiSharp.Classes.ResponseWrappers;
+using InstagramApiSharp.Converters;
+using InstagramApiSharp.Enums;
+using InstagramApiSharp.Helpers;
+using InstagramApiSharp.Logger;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramApiSharp.API.Services
+{
+    internal class RegistrationService : IRegistrationService
+    {
+
+        #region Fields and constructor
+        private readonly AndroidDevice _deviceInfo;
+        private readonly IHttpRequestProcessor _httpRequestProcessor;
+        private readonly IInstaLogger _logger;
+        private readonly UserSessionData _user;
+        private readonly UserAuthValidate _userAuthValidate;
+        private readonly InstaApi _instaApi;
+        private readonly HttpHelper _httpHelper;
+
+        public RegistrationService(AndroidDevice deviceInfo, UserSessionData user,
+            IHttpRequestProcessor httpRequestProcessor, IInstaLogger logger,
+            UserAuthValidate userAuthValidate, InstaApi instaApi, HttpHelper httpHelper)
+        {
+            _deviceInfo = deviceInfo;
+            _user = user;
+            _httpRequestProcessor = httpRequestProcessor;
+            _logger = logger;
+            _userAuthValidate = userAuthValidate;
+            _instaApi = instaApi;
+            _httpHelper = httpHelper;
+        }
+        #endregion Properties and constructor
+    }
+}
