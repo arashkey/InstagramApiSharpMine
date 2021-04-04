eaab736a6e2ba5b99cc6a03b0d203858ac405aad
ramtin <ramtinak@live.com>
2021-04-04 19:51:05 +0430

    Add InstaAccountContactPoint

---
+++ src/InstagramApiSharp/Classes/Models/Other/InstaAccountContactPoint.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Developer: Ramtin Jokar [ Ramtinak@live.com ] [ My Telegram Account: https://t.me/ramtinak ]
+ * 
+ * Github source: https://github.com/ramtinak/InstagramApiSharpMine
+ * 
+ * IRANIAN DEVELOPERS (c) 2021, April
+ */
+
+using InstagramApiSharp.Classes.Models;
+using Newtonsoft.Json;
+
+namespace InstagramApiSharp.Classes
+{
+    public partial class InstaAccountContactPoint : InstaDefaultResponse
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("phone_number")]
+        public string PhoneNumber { get; set; }
+    }
+}
