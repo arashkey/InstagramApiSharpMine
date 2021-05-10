17552dffb14cd4a61e77e9fa9e0f6fff1e7b30b1
ramtin <ramtinak@live.com>
2021-05-10 20:13:19 +0430

    Remove older android versions

--- src/InstagramApiSharp/Classes/Android/DeviceInfo/AndroidVersionList.cs
+++ src/InstagramApiSharp/Classes/Android/DeviceInfo/AndroidVersionList.cs
@@ -12,42 +12,6 @@ namespace InstagramApiSharp.Classes.Android.DeviceInfo
             {
                 new AndroidVersion
                 {
-                    Codename = "Lollipop",
-                    VersionNumber = "5.1.0",
-                    APILevel = "22"
-                },
-                new AndroidVersion
-                {
-                    Codename = "Marshmallow",
-                    VersionNumber = "6.0.0",
-                    APILevel = "23"
-                },
-                new AndroidVersion
-                {
-                    Codename = "Nougat",
-                    VersionNumber = "7.0.0",
-                    APILevel = "24"
-                },
-                new AndroidVersion
-                {
-                    Codename = "Nougat",
-                    VersionNumber = "7.1.0",
-                    APILevel = "25"
-                },
-                new AndroidVersion
-                {
-                    Codename = "Oreo",
-                    VersionNumber = "8.0.0",
-                    APILevel = "26"
-                },
-                new AndroidVersion
-                {
-                    Codename = "Oreo",
-                    VersionNumber = "8.1.0",
-                    APILevel = "27"
-                },
-                new AndroidVersion
-                {
                     Codename = "Pie",
                     VersionNumber = "9.0.0",
                     APILevel = "28"
