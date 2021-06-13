79e423872aa69bb0392c50a3f4f32ee671e3e0dd
ramtin <ramtinak@live.com>
2021-06-13 17:04:22 +0430

    Tiny update

--- src/InstagramApiSharp/Classes/StateData.cs
+++ src/InstagramApiSharp/Classes/StateData.cs
@@ -21,42 +21,14 @@ namespace InstagramApiSharp.Classes
 
 
         #region Locale
-        /// <summary>
-        ///     Default is -14400
-        /// </summary>
-        public int TimezoneOffset { get; set; }
-        /// <summary>
-        ///     X-IG-App-Startup-Country header
-        ///     <para>Default is US</para>
-        /// </summary>
-        public string StartupCountry { get; set; }
-        /// <summary>
-        ///     X-IG-Timezone-Offset header
-        ///     <para>Default is choosen from your system information</para>
-        /// </summary>
-        public uint StartupCountryCode { get; set; }
-        /// <summary>
-        ///     X-IG-Device-Locale header
-        ///     <para>Default is en_US</para>
-        /// </summary>
-        public string DeviceLocale { get; set; }
-        /// <summary>
-        ///     X-IG-App-Locale header
-        ///     <para>Default is en_US</para>
-        /// </summary>
-        public string AppLocale { get; set; }
-        /// <summary>
-        ///     X-IG-Mapped-Locale
-        ///     <para>Default is en_US</para>
-        /// </summary>
-        public string MappedLocale { get; set; }
-        /// <summary>
-        ///     Accept-Language
-        ///     <para>Default is en-US</para>
-        ///     <para>for UK should be: en-GB, en-US</para>
-        /// </summary>
-        public string AcceptLanguage { get; set; }
 
+        public int TimezoneOffset { get; set; }
+        public string StartupCountry { get; set; }
+        public uint StartupCountryCode { get; set; }
+        public string DeviceLocale { get; set; }
+        public string AppLocale { get; set; }
+        public string MappedLocale { get; set; }
+        public string AcceptLanguage { get; set; }
 
         #endregion
     }
