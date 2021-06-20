4436964c08615b0f814cbef6d803d7fc84c4a3ad
ramtin <ramtinak@live.com>
2021-06-20 14:19:15 +0430

    Add Insta2FANotificationReviewStatus to InstaTwoFactorTrustedNotification

---
+++ src/InstagramApiSharp/Enums/Insta2FANotificationReviewStatus.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Developer: Ramtin Jokar [ Ramtinak@live.com ] [ My Telegram Account: https://t.me/ramtinak ]
+ * 
+ * Github source: https://github.com/ramtinak/InstagramApiSharpMine
+ * 
+ * IRANIAN DEVELOPERS
+ */
+namespace InstagramApiSharp.Enums
+{
+    public enum Insta2FANotificationReviewStatus
+    {
+        Unchanged = 0,
+        Approved = 1,
+        Denied = 2
+    }
+}
