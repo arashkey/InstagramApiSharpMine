470208ec4d0aff7218a32385502a22b094446af0
ramtinak <ramtinak@live.com>
2021-09-25 01:19:02 +0330

    Add MusicProcessor to IInstaApi

---
+++ src/InstagramApiSharp/API/Processors/IMusicProcessor.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Developer: Ramtin Jokar [ Ramtinak@live.com ] [ My Telegram Account: https://t.me/ramtinak ]
+ * 
+ * Github source: https://github.com/ramtinak/InstagramApiSharpMine
+ * 
+ * IRANIAN DEVELOPERS
+ */
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using InstagramApiSharp.Classes;
+using InstagramApiSharp.Classes.Models;
+using InstagramApiSharp.Enums;
+
+namespace InstagramApiSharp.API.Processors
+{
+    /// <summary>
+    ///     Represents Instagram Music APIs
+    /// </summary>
+    public interface IMusicProcessor
+    {
+    }
+}
